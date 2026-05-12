7ae2bc0bdb4669132a8f14ce5120b7bd4c1c6e1c
Guaidaoyiyoudao <728975026@qq.com>
2026-05-12 15:46:07 +0800

    Remove primary tab floating add action

--- app/src/main/java/com/stock/dividend/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/stock/dividend/ui/screen/HomeScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
